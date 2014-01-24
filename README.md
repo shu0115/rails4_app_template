1e365dcc8bbd687fd83701d69707c5f44ad7d964
shu0115 <s.matsumoto0115@gmail.com>
2014-01-24 11:02:16 +0900

    テンプレート記述

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-rails4_app_template
-===================
+# Rails4 App Template
+
+### Usage
+
+```bash
+rails new new_app_name -m https://raw.github.com/shu0115/rails4_app_template/master/app_template.rb
+----------
+Overwrite /Users/shu/labo/new_app_name/Gemfile? (enter "h" for help) [Ynaqdh] a
+----------
+```
---
+++ app_template.rb
@@ -0,0 +1,53 @@
+# インストールGem
+gem 'minimum-omniauth-scaffold'
+gem 'action_args'
+gem 'html5_validators'
+gem 'puma'
+gem 'kaminari'
+gem 'rails_config'
+gem 'tapp'
+gem 'awesome_print'
+gem 'i18n_generators'
+gem "exception_notification"
+gem 'haml-rails'
+
+# Markdown & Syntax Highlight
+gem 'redcarpet'
+gem 'coderay'
+
+# Development
+gem_group :development do
+  gem "heroku_san"
+  gem 'pry-rails'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem "erb2haml"
+  gem 'quiet_assets'
+  gem 'colorize_unpermitted_parameters'
+  gem 'rails-flog-disable-sql-format', require: 'flog'
+  gem 'bullet'
+end
+
+# Development AND Test
+gem_group :development, :test do
+  gem "sqlite3"
+  gem 'rspec-rails'
+end
+
+# Production
+gem_group :production do
+  gem 'rails_12factor'
+  gem 'pg'
+  gem 'newrelic_rpm'
+end
+
+# CommentOut
+comment_lines 'Gemfile', "gem 'sqlite3'"
+
+# Gem Install
+run "bundle install --without production"
+run "bundle update"
+
+# Generate Command
+run "rails generate heroku_san"
+run "rails generate rspec:install"
